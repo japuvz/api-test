845a5151e801918fca4633ec5770b782fe8054d7
japuvz <lmarant1@umbc.edu>
2024-01-21 20:45:12 -0500

    Add Item model and post method

--- app/main.py
+++ app/main.py
@@ -5,3 +5,25 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+
+# Create a post method
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     return item
+
+# Create Item model using Pydantic
+from typing import Optional
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+
+
+
+
+
